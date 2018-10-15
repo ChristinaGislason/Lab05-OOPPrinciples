18772b11e79016c26645675b51a1820d0dca6bb4
Christina Gislason <christinamkcho@gmail.com>
2018-10-15 11:26:28 -0700

    create animal class and new animal object

---
+++ Lab05-OOPPrinciples/Lab05-OOPPrinciples/Classes/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab05_OOPPrinciples.Classes
+{
+    class Animal
+    {
+        public string Mammal { get; set; }
+        public string Aviary { get; set; }
+        public string Crustacean { get; set; }
+
+    }
+}
---
+++ Lab05-OOPPrinciples/Lab05-OOPPrinciples/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab05_OOPPrinciples.Classes;
+using System;
+
+namespace Lab05_OOPPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string Animal()
+        {
+            // instantiate a new animal object
+            Animal animal = new Animal();
+
+            animal.Mammal = "feline";
+            animal.Aviary = "tropical bird";
+            animal.Crustacean = "shrimp";
+        }
+    }
+}
